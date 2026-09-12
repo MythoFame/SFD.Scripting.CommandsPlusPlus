240930245ae076f03fb99e147fef831aa26b3be0
HAZARD <ghostyn678+git@gmail.com>
2026-09-12 11:43:57 -0300

    init

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using SFDGameScriptInterface;
+
+namespace SFD.Scripting.CommandsPlusPlus;
+
+public abstract class GameScriptInterfaceExtended : GameScriptInterface
+{
+    protected static readonly IGame Game;
+}
+
+public partial class GameScript : GameScriptInterfaceExtended
+{
+    // ...
+}
