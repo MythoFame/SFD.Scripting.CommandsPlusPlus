7dfc3d665575169df826a3cd070e748e04d13c48
HAZARD <ghostyn678+git@gmail.com>
2026-09-14 08:38:22 -0300

    feat: add player teleport command
    Add /tp command that teleports players to another player or to the command
    executor if no target is provided. Command is moderator-only.

--- TODO.md
+++ TODO.md
@@ -55,7 +55,7 @@ Interact with players. Moderator-only.
 - [x] `/kill <player> [gib|remove]`: Kills a player, optionally gibbing them or removing them from the game.
 - [ ] `/revive <player>`: Revives a dead player.
 - [ ] `/input <player>`: Toggles whether a player can provide input, effectively freezing or unfreezing them.
-- [ ] `/tp <from> [to]`: Teleports a player to another player. If no target is provided, teleports them to your position.
+- [x] `/tp <from> [to]`: Teleports a player to another player. If no target is provided, teleports them to your position.
 - [ ] `/team <player> <team>`: Sets the team of a player.
 - [ ] `/trip <player>`: Trips a player, knocking them down.
 - [ ] `/setmodifier <player> <modifier> <value>`: Sets a player modifier to the given value.
--- Utils/PlayerModule.cs
+++ Utils/PlayerModule.cs
@@ -19,6 +19,9 @@ public partial class GameScript : GameScriptInterfaceExtended
             AddCommand("kill", Kill,
                 "- Kills a player, optionally gibbing or removing them",
                 moderatorOnly: true);
+            AddCommand("tp", Tp,
+                "- Teleports a player to another player, or to you if no target is given",
+                moderatorOnly: true);
         }
 
         public override void OnEnable()
@@ -78,6 +81,76 @@ public partial class GameScript : GameScriptInterfaceExtended
                 : "Killed";
 
             Game.ShowChatMessage($"{verb} {affected} player(s).", Color.Green, args.User.UserIdentifier);
+        }
+
+        private static void Tp(UserMessageCallbackArgs args)
+        {
+            string[] tokens = [.. ParseHelper.SplitArguments(args.CommandArguments)];
+
+            if (tokens.Length == 0 || tokens.Length > 2)
+            {
+                Game.ShowChatMessage("Usage: /tp <from> [to]", Color.Red, args.User.UserIdentifier);
+                return;
+            }
+
+            IPlayer[] sources = [.. ParseHelper.ParsePlayers(tokens[0], args.User)];
+
+            if (sources.Length == 0)
+            {
+                Game.ShowChatMessage($"Player '{tokens[0]}' not found.", Color.Red, args.User.UserIdentifier);
+                return;
+            }
+
+            Vector2 targetPos;
+            string targetLabel;
+
+            if (tokens.Length == 2)
+            {
+                IPlayer target = null;
+
+                foreach (IPlayer candidate in ParseHelper.ParsePlayers(tokens[1], args.User))
+                {
+                    if (candidate != null && !candidate.IsRemoved)
+                    {
+                        target = candidate;
+                        break;
+                    }
+                }
+
+                if (target == null)
+                {
+                    Game.ShowChatMessage($"Player '{tokens[1]}' not found.", Color.Red, args.User.UserIdentifier);
+                    return;
+                }
+
+                targetPos = target.GetWorldPosition();
+                targetLabel = target.Name;
+            }
+            else
+            {
+                IPlayer self = args.User.GetPlayer();
+
+                if (self == null || self.IsRemoved)
+                {
+                    Game.ShowChatMessage("You have no live player to teleport to.", Color.Red, args.User.UserIdentifier);
+                    return;
+                }
+
+                targetPos = self.GetWorldPosition();
+                targetLabel = "you";
+            }
+
+            int affected = 0;
+
+            foreach (IPlayer player in sources)
+            {
+                if (player == null || player.IsRemoved) continue;
+
+                player.SetWorldPosition(targetPos);
+                affected++;
+            }
+
+            Game.ShowChatMessage($"Teleported {affected} player(s) to {targetLabel}.", Color.Green, args.User.UserIdentifier);
         }
     }
 }
