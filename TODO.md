a2fd26d43f99f78ed29075b328059f8efcda38a0
HAZARD <ghostyn678+git@gmail.com>
2026-09-14 11:56:50 -0300

    feat: add refill command
    Add the /refill command to allow moderators to refill a player's ammo as if they used an ammo stash.

--- TODO.md
+++ TODO.md
@@ -68,7 +68,7 @@ Interact with players. Moderator-only.
 - [ ] `/user <from> <to>`: Sets one player's user to another player's user.
 - [ ] `/wear <name> <type> [primary_color] [secondary_color]`: Gives your profile a cosmetic item of the given type and colors.
 - [ ] `/action <player>`: Queues an action for a player whose input is disabled.
-- [ ] `/refill <player>`: Refills a player's ammo as if they used an ammo stash.
+- [x] `/refill <player>`: Refills a player's ammo as if they used an ammo stash.
 - [ ] `/magnet <player> [area_size] [players|objects]`: Toggles attraction of nearby players and/or objects toward a player.
 - [ ] `/repulse <player> [area_size] [players|objects]`: Toggles repulsion of nearby players and/or objects away from a player.
 
--- Utils/PlayerModule.cs
+++ Utils/PlayerModule.cs
@@ -40,6 +40,9 @@ public partial class GameScript : GameScriptInterfaceExtended
             AddCommand("revive", Revive,
                 "<player> - Revives a dead player",
                 moderatorOnly: true);
+            AddCommand("refill", Refill,
+                "<player> - Refills a player's ammo as if they used an ammo stash",
+                moderatorOnly: true);
             AddCommand("fly", FlyCommand,
                 "<player> - Toggles flying for a player",
                 moderatorOnly: true);
@@ -411,6 +414,41 @@ public partial class GameScript : GameScriptInterfaceExtended
             {
                 Game.ShowChatMessage($"{skipped} player(s) were not dead.", Color.Yellow, args.User.UserIdentifier);
             }
+        }
+
+        private static void Refill(UserMessageCallbackArgs args)
+        {
+            string[] tokens = [.. ParseHelper.SplitArguments(args.CommandArguments)];
+
+            if (tokens.Length != 1)
+            {
+                Game.ShowChatMessage("Usage: /refill <player>", Color.Red, args.User.UserIdentifier);
+                return;
+            }
+
+            IPlayer[] players = [.. ParseHelper.ParsePlayers(tokens[0], args.User)];
+
+            if (players.Length == 0)
+            {
+                Game.ShowChatMessage($"Player '{tokens[0]}' not found.", Color.Red, args.User.UserIdentifier);
+                return;
+            }
+
+            int affected = 0;
+
+            foreach (IPlayer player in players)
+            {
+                if (player == null || player.IsRemoved) continue;
+
+                player.SetCurrentPrimaryWeaponAmmo(int.MaxValue);
+                player.SetCurrentSecondaryWeaponAmmo(int.MaxValue);
+                player.SetCurrentThrownItemAmmo(int.MaxValue);
+                affected++;
+            }
+
+            if (affected == 0) return;
+
+            Game.ShowChatMessage($"Refilled ammo for {affected} player(s).", Color.Green, args.User.UserIdentifier);
         }
 
         private static void FlyCommand(UserMessageCallbackArgs args)
